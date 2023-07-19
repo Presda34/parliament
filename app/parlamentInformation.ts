410775eda567cd1f722ac42512acdc3671d6334d
Michael Firlus <michael@firlus.dev>
2023-07-19 10:14:46 +0200

    📄Regarding #3: Added Italy

--- app/parlamentInformation.ts
+++ app/parlamentInformation.ts
@@ -1,7 +1,8 @@
 import { de } from './parliaments/de';
 import { fr } from './parliaments/fr';
+import { it } from './parliaments/it';
 import { gb } from './parliaments/gb';
 import { ru } from './parliaments/ru';
 import { tr } from './parliaments/tr';
 
-export const parliamentInformation = [de, fr, ru, tr, gb];
+export const parliamentInformation = [fr, de, it, ru, tr, gb];
---
+++ app/parliaments/it.ts
@@ -0,0 +1,84 @@
+export const it = {
+  country: 'it',
+  name: 'Camera dei deputati',
+  factions: [
+    {
+      name: `Fratelli d'Italia (FdI)`,
+      color: '#0b3b6a',
+      deputies: [
+        { name: `Fratelli d'Italia (FdI)`, deputies: 117 },
+        { name: 'Verde è Popolare (VèP)', deputies: 1 },
+      ],
+    },
+    {
+      name: `Partito Democratico - Italia Democratica e Progressista (PD-IDP)`,
+      color: '#ff4040',
+      deputies: [
+        { name: `Partito Democratico (PD)`, deputies: 67 },
+        { name: 'Democrazia Solidale (DemoS)', deputies: 1 },
+        { name: 'Centro Democratico (CD)', deputies: 1 },
+      ],
+    },
+    {
+      name: 'Lega',
+      color: '#203087',
+      deputies: 66,
+    },
+    {
+      name: 'Movimento Cinque Stelli (M5S)',
+      color: '#ffff00',
+      deputies: 52,
+    },
+    {
+      name: `Forza Italia - Berlusconi Presidente - PPE`,
+      color: '#00a2e8',
+      deputies: [
+        { name: `Forza Italia (FI)`, deputies: 42 },
+        { name: 'Nuovo Partito Socialista Italiano (NPSI)', deputies: 1 },
+        { name: 'Sud in Testa (SiT)', deputies: 1 },
+      ],
+    },
+    {
+      name: `Azione - Italia Viva (A-IV-RE)`,
+      color: '#cc3c84',
+      deputies: [
+        { name: `Azione (Az)`, deputies: 11 },
+        { name: 'Italia Viva (IV)', deputies: 10 },
+      ],
+    },
+    {
+      name: `Alleanza Verdi e Sinistra (AVS)`,
+      color: '#54c363',
+      deputies: [
+        { name: `Europa Verde (EV)`, deputies: 6 },
+        { name: 'Sinistra Italiana (SI)', deputies: 4 },
+        { name: 'Partito Progressista (PP)', deputies: 1 },
+      ],
+    },
+    {
+      name: `Noi moderati/Lupi - Toti - Brugnaro - Udc - Movimento Associativo Italiani all'Estero (NM(N-I-C-U)-MAIE)`,
+      color: '#fc0fc0',
+      deputies: [
+        { name: `Noi con l'Italia (NcI)`, deputies: 3 },
+        { name: 'Italia al Centro (IaC)', deputies: 2 },
+        { name: 'Corragio Italia (CI)', deputies: 1 },
+        { name: 'Unione di Centro (PP)', deputies: 1 },
+        { name: `Movimento Associativo Italiani all'Estero (MAIE)`, deputies: 1 },
+        { name: 'Movimento Animalista (MA)', deputies: 1 },
+        { name: 'Independent', deputies: 1 },
+      ],
+    },
+    {
+      name: `Gruppo misto`,
+      color: '#aaaaaa',
+      deputies: [
+        { name: `Südtiroler Volkspartei/Partito Autonomista Trentino Tirolese (SVP-PATT)`, deputies: 3 },
+        { name: 'Union Valdôtaine (UV)', deputies: 1 },
+        { name: '+Europa (+EU)', deputies: 2 },
+        { name: 'Partito Democratico', deputies: 1 },
+        { name: 'Independent', deputies: 2 },
+      ],
+    },
+  ],
+  pollTypes: [],
+};
