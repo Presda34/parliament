d854768493b17dcdbee3fd3c4e826e81025fbb6e
Michael Firlus <michael@firlus.dev>
2023-07-19 16:09:46 +0200

    🐛 Closes #17: navigator only loading on client side

--- components/flagEmoji.tsx
+++ components/flagEmoji.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Noto_Color_Emoji } from "next/font/google";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 type Props = {
   children: ReactNode
@@ -10,7 +10,13 @@ type Props = {
 const notoColorEmoji = Noto_Color_Emoji({ subsets: ['emoji'], weight: "400" });
 
 export default function FlagEmoji({ children }: Props) {
-  const needIconFont = navigator && (navigator.userAgent.includes('Win') && navigator.userAgent.includes('Chrom'));
+
+  const [needIconFont, setNeedIconFont] = useState(false);
+
+  useEffect(() => {
+    setNeedIconFont(navigator && (navigator.userAgent.includes('Win') && navigator.userAgent.includes('Chrom')));
+  }, [])
+
   return <div className={needIconFont ? notoColorEmoji.className : ''}>
     {children}
   </div>
