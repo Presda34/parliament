d35e440987ada495915573905588fe225f2cef6b
Michael Firlus <michael@firlus.dev>
2023-07-19 13:48:00 +0200

    📄 Regarding #3: Added Ukraine

--- app/parlamentInformation.ts
+++ app/parlamentInformation.ts
@@ -5,5 +5,6 @@ import { gb } from './parliaments/gb';
 import { ru } from './parliaments/ru';
 import { es } from './parliaments/es';
 import { tr } from './parliaments/tr';
+import { ua } from './parliaments/ua';
 
-export const parliamentInformation = [fr, de, it, ru, es, tr, gb];
+export const parliamentInformation = [fr, de, it, ru, es, tr, ua, gb];
---
+++ app/parliaments/ua.ts
@@ -0,0 +1,64 @@
+export const ua = {
+  country: 'ua',
+  name: 'Verkhovna Rada',
+  factions: [
+    {
+      name: 'Sluha narodu (SN)',
+      color: '#38b34a',
+      deputies: 237,
+    },
+    {
+      name: `Yevropeys'ka solidarnist' (YeS)`,
+      color: '#1b3892',
+      deputies: 27,
+    },
+    {
+      name: `Bat'kivshchyna`,
+      color: '#ed1c24',
+      deputies: 24,
+    },
+    {
+      name: 'Platforma za zhyttia ta myr (PZZhM)',
+      color: '#21409a',
+      deputies: 22,
+    },
+    {
+      name: 'Holos',
+      color: '#fa4616',
+      deputies: [
+        { name: `Spravedlyvist'`, deputies: 11 },
+        { name: 'Holos', deputies: 9 },
+      ],
+    },
+    {
+      name: 'Za majbutnije (ZM)',
+      color: '#5c068c',
+      deputies: [
+        { name: `Za majbutnije (ZM)`, deputies: 11 },
+        { name: `Ukrainske ob'ednannya patriotiv (UKROP)`, deputies: 3 },
+        { name: `Nash krai`, deputies: 2 },
+        { name: `Ahrarna partiya Ukrayiny`, deputies: 1 },
+        { name: `Komanda Andriya Balohy'`, deputies: 1 },
+      ],
+    },
+    {
+      name: 'Doriva',
+      color: '#1a9daa',
+      deputies: [
+        { name: `Doriva`, deputies: 16 },
+        { name: `Bila Tserkva razom`, deputies: 1 },
+        { name: `Samopomich`, deputies: 1 },
+      ],
+    },
+    {
+      name: 'Vidnovlennya Ukrayiny',
+      color: '#ffdd00',
+      deputies: 18,
+    },
+    {
+      name: 'Independent',
+      color: '#aaaaaa',
+      deputies: 21,
+    },
+  ],
+};
