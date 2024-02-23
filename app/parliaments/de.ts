1dcf3940a2ce99cea7fc06f87faf3a18353552af
Michael Firlus <michael@diewebsitemacherei.de>
2024-02-23 11:05:47 +0100

    📈 Updated data for Germany

--- app/parliaments/de.ts
+++ app/parliaments/de.ts
@@ -1,50 +1,64 @@
 export const de = {
-  country: 'de',
-  name: 'Bundestag',
+  country: "de",
+  name: "Bundestag",
   factions: [
     {
-      name: 'Sozialdemokratische Partei Deutschland (SPD)',
-      color: '#eb001f',
-      deputies: 206,
+      name: "Sozialdemokratische Partei Deutschland (SPD)",
+      color: "#eb001f",
+      deputies: 207,
     },
     {
-      name: 'Union',
-      color: '#161a1d',
+      name: "Union",
+      color: "#161a1d",
       deputies: [
-        { name: 'Christlich Demokratische Union', deputies: 152 },
-        { name: 'Christlich-Soziale Union', deputies: 45 },
+        { name: "Christlich Demokratische Union", deputies: 152 },
+        { name: "Christlich-Soziale Union", deputies: 45 },
       ],
     },
     {
-      name: 'Bündnis 90/Die Grünen',
-      color: '#64a12d',
+      name: "Bündnis 90/Die Grünen",
+      color: "#64a12d",
       deputies: 118,
     },
     {
-      name: 'Freie Demokratische Partei (FDP)',
-      color: '#ffed00',
+      name: "Freie Demokratische Partei (FDP)",
+      color: "#ffed00",
       deputies: 92,
     },
     {
-      name: 'Alternative für Deutschland (AfD)',
-      color: '#009ee0',
+      name: "Alternative für Deutschland (AfD)",
+      color: "#009ee0",
       deputies: 78,
     },
     {
-      name: 'Die Linke',
-      color: '#be3075',
-      deputies: 39,
+      name: "Die Linke",
+      color: "#be3075",
+      deputies: 28,
     },
     {
-      name: 'Südschleswigsche Wählerverband (SSW)',
-      color: '#003c8f',
-      deputies: 1,
+      name: "Bündnis Sahra Wagenknecht (BSW)",
+      color: "#7D254F",
+      deputies: 10,
     },
     {
-      name: 'Independent',
-      color: '#aaaaaa',
-      deputies: 5,
+      name: "Unaffiliated",
+      color: "#aaaaaa",
+      deputies: [
+        {
+          name: "Südschleswigsche Wählerverband (SSW)",
+          deputies: 1,
+        },
+        {
+          name: "Alternative für Deutschland (AfD)",
+          deputies: 1,
+        },
+        {
+          name: "Independent",
+          deputies: 4,
+        },
+      ],
     },
   ],
   pollTypes: [],
 };
+
